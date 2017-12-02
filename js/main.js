8f76beea13c01c7e1eb26afc8104701d9cd17f8e
mouyaq <mouyaq@gmail.com>
2017-12-02 12:27:23 +0100

    fix: clean main.js

--- js/main.js
+++ js/main.js
@@ -8,17 +8,13 @@ window.onload = function() {
 function startGame() {
     var canvas = document.getElementById("game");
     var game = new Game(canvas);
-    canvas.height = 800;
     window.addEventListener('resize', resizeCanvas, false);
     function resizeCanvas() {
         canvas.width = window.innerWidth;
-        canvas.height = 800;
+        canvas.height = window.innerHeight;
         
     }
     resizeCanvas();
-    // canvas.width = "100%";
-    // canvas.height = "800px";
-    // var ctx = canvas.msGetInputContext("2d");
     game.draw();
     
 }
